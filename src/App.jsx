49155dc2c20f1a15aa93d2bb6b57e54cff0b13cc
Manishpandey10 <manish2310pandey@gmail.com>
2024-03-10 18:45:13 +0530

    Authorisation

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@
 import './App.css'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
 
   return (
     <>
     <h1>A blog app</h1>
-    <h1>A hello g app</h1>
+    
     </>
   )
 }
---
+++ src/appwrite/auth_service.js
@@ -0,0 +1,51 @@
+import conf from "../config/config";
+import {Client, Account, ID} from 'appwrite'
+
+
+export class Authwrite{
+    client = new Client()
+    account;
+
+    constructor(){
+        this.client
+                .setEndpoint(conf.appwrite_url)
+                .setProject(conf.appwrite_projectid)
+        this.account= new Account(this.client)        
+    }
+
+    async createAccount({email, password, name }){
+        try{
+            const userAccount = await this.account.create(ID.unique(), email, name )
+            if (userAccount)
+            {
+                return this.login({email,password});
+            }
+            else{
+                return userAccount
+            }
+        }
+        catch(error){
+            throw error
+        }
+
+    }
+    async login({email, password}){
+        try {
+            const login= this.account.createEmailPasswordSession(email,password)
+            return login
+        } catch (error) {
+            throw error
+        }
+    }
+    async getUserAccount(){
+        try {
+            return await this.account.get()
+        } catch (error) {
+            console.log("no user present here !!!!")
+        }
+    }
+}
+
+const authWrite = new Authwrite()
+
+export default authWrite
